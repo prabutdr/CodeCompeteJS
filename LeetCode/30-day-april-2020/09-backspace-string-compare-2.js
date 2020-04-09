18bb831225593b3d42b27b8048f2ce07794b5f4e
prabutdr <prabutdr@gmail.com>
2020-04-09 19:34:03 +0530

    Day 09 challenge completed

---
+++ LeetCode/30-day-april-2020/09-backspace-string-compare-2.js
@@ -0,0 +1,27 @@
+// O(N) time and O(N) space
+/**
+ * @param {string} S
+ * @param {string} T
+ * @return {boolean}
+ */
+var backspaceCompare = function(S, T) {
+  return revisedString(S) === revisedString(T);
+};
+
+function revisedString(str) {
+  const stack = [];
+
+  for (let ch of str) {
+    if (ch === '#')
+      stack.pop();
+    else 
+      stack.push(ch);
+  }
+  return stack.join('');
+}
+
+console.log(backspaceCompare("ab#c", "ad#c"));
+console.log(backspaceCompare("ab##", "c#d#"));
+console.log(backspaceCompare("a##c", "#a#c"));
+console.log(backspaceCompare("a#c", "b"));
+console.log(backspaceCompare("bxj##tw", "bxo#j##tw"));
---
+++ LeetCode/30-day-april-2020/09-backspace-string-compare.js
@@ -0,0 +1,40 @@
+// O(N) time and O(1) space
+/**
+ * @param {string} S
+ * @param {string} T
+ * @return {boolean}
+ */
+var backspaceCompare = function(S, T) {
+    let scp = nextChar(S);
+    let tcp = nextChar(T);
+
+    while (scp[0] >= 0 && tcp[0] >= 0 && scp[1] === tcp[1]) {
+      scp = nextChar(S, scp[0] - 1);
+      tcp = nextChar(T, tcp[0] - 1);
+    }
+
+    return scp[0] < 0 && tcp[0] < 0;
+};
+
+function nextChar(str, i = str.length - 1, backspaceCount = 0) {
+  while (i >= 0 && str[i] === '#') {
+    backspaceCount++;
+    i--;
+  }
+  
+  while (backspaceCount > 0 && str[i] !== '#') {
+    i--;
+    backspaceCount--;
+  }
+
+  if (backspaceCount > 0)
+    return nextChar(str, i, backspaceCount);
+
+  return str[i] === '#'? nextChar(str, i): [i, str[i]];
+}
+
+console.log(backspaceCompare("ab#c", "ad#c"));
+console.log(backspaceCompare("ab##", "c#d#"));
+console.log(backspaceCompare("a##c", "#a#c"));
+console.log(backspaceCompare("a#c", "b"));
+console.log(backspaceCompare("bxj##tw", "bxo#j##tw"));
